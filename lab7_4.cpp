da422fcf8fe5115b07a1f76e85a29a02e72fa82a
TelTriNo <natanan_tatanan@cmu.ac.th>
2022-12-13 17:33:23 +0700

    lab7_4 complete

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -8,7 +8,7 @@ int main(){
 	string name[] = {"Luffy","Zoro","Sanji","Nami","Naruto","Sakura","All Might","Saitama","Eren","Mikasa"};
 	int id[] = {123,196,225,329,420,451,515,666,690,787};
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
-	double grade;
+	double grade;              
 	int i=0;
 
     cout << "Please input GPA: ";
